72be9cc2c5549db7babf8c7124c33d6922be36b6
mandos1995 <mandos19950602@gmail.com>
2024-04-15 22:47:36 +0900

    BOJ 1008 sol

---
+++ 1000 ~ 1999/1008_A나누기B.cpp
@@ -0,0 +1,8 @@
+//
+//  B.cpp
+//  Cpp
+//
+//  Created by 김민석 on 4/15/24.
+//
+
+#include <stdio.h>
---
+++ 1000 ~ 1999/1008_A나누기B.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long double a, b;
+    cin >> a >> b;
+    cout.precision(16);
+    cout << a/b;
+}
