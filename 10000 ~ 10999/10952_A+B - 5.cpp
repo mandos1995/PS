8c2e8fe2e8195783692a972501a5f1b198235b0a
mandos1995 <mandos19950620@gmail.com>
2024-05-06 19:25:22 +0900

    BOJ 10952 sol

---
+++ 10000 ~ 10999/10952_A+B - 5.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    while (true) {
+        int a, b;
+        cin >> a >> b;
+        if (a == 0 && b == 0) { break; }
+        cout << a + b << "\n";
+    }
+}
