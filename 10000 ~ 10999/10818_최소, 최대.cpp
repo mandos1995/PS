6aa8fc003482b564367652f4f9cb55ec6b65f6d6
mandos1995 <mandos19950620@gmail.com>
2024-05-10 23:40:58 +0900

    BOJ 10818 sol

---
+++ 10000 ~ 10999/10818_최소, 최대.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    cout << *min_element(arr, arr + n) << " " << *max_element(arr, arr + n);
+}
