14edd0a4e7e8a3708e32531c85bd2b62d25966b0
mandos1995 <mandos19950620@gmail.com>
2024-05-27 23:37:30 +0900

    BOJ 27866 sol

--- 27000 ~ 27999/27866_문자와 문자열.cpp
+++ 27000 ~ 27999/27866_문자와 문자열.cpp
@@ -1,8 +1,11 @@
-//
-//  27866_문자와 문자열.cpp
-//  Cpp
-//
-//  Created by 김민석 on 5/27/24.
-//
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <stdio.h>
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    int n;
+    cin >> s >> n;
+    cout << s[n-1];
+}
