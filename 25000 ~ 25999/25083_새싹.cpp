5bab6a42682bdcd019e34b1fe23bba17a14a155e
mandos1995 <mandos19950620@gmail.com>
2024-05-27 23:33:44 +0900

    BOJ 25083 sol

---
+++ 25000 ~ 25999/25083_새싹.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |";
+}
