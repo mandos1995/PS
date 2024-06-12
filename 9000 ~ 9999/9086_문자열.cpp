77009eea07159a4dba58e18c7041e03cccaccaec
mandos1995 <mandos19950620@gmail.com>
2024-06-12 23:28:06 +0900

    BOJ 9086 sol

---
+++ 9000 ~ 9999/9086_문자열.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    while(n--) {
+        string s;
+        cin >> s;
+        cout << s.front() << s.back() << "\n";
+    }
+}
